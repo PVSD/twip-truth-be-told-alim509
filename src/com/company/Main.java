7769b1255bd0dc7a9cddf8ea3981d8de30c793d6
al5091 <angelovinolim@gmail.com>
2018-12-10 11:21:23 -0500

    wack

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner kbInput = new Scanner(System.in);
+
+        boolean A;
+        boolean B;
+        boolean test = kbInput.nextBoolean();
+
+        for (int i = 0; i < 4; i++) {
+            if (i == 0) {
+                A = true;
+                B = true;
+            } else if (i == 1) {
+                A = true;
+                B = false;
+            } else if (i == 2) {
+                A = false;
+                B = true;
+            } else {
+                A = false;
+                B = false;
+            }
+
+            test = A && B;
+            System.out.println(test);
+
+        }
+
     }
 }
